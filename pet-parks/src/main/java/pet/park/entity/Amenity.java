c07bda728dd73117431c3e090523a1f9301d1ab7
Joseph <68339401+jo30231619@users.noreply.github.com>
2023-12-18 21:09:11 -0500

    second commit

---
+++ pet-parks/src/main/java/pet/park/entity/Amenity.java
@@ -0,0 +1,26 @@
+package pet.park.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class Amenity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long amenityId;
+	
+	private String amenity;
+	
+	@ManyToMany(mappedBy = "amenities")
+	private Set<PetPark> petParks = new HashSet<>();
+}
+
+//This is how JPA manages relationships, there must be the table in both entities
